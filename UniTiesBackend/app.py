9ecfbb9a2692b1a30f374f0c944ddf9e3d2b29c4
OnigiriRolls <szilvia.bogadi@student.upt.ro>
2023-11-18 12:28:08 +0200

    venv

--- UniTiesBackend/app.py
+++ UniTiesBackend/app.py
@@ -3,12 +3,13 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['demo']
-collection = db['data']
+client = MongoClient('localhost', 27017, username='User', password='4lNukg1PfxSL8SZj')
+db = client['UniStud']
+collection = db['sample_mflix']
 
 @app.route('/')
 def hello_world():  # put application's code here
+    data = collection.find('movies')
     return 'Hello World!'
 
 
