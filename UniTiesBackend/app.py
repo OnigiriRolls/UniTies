5310c2accc9c064e61057e348f7c10eaf1150d3a
OnigiriRolls <szilvia.bogadi@student.upt.ro>
2023-11-18 15:26:56 +0200

    Added get method example

--- UniTiesBackend/app.py
+++ UniTiesBackend/app.py
@@ -1,16 +1,65 @@
-from flask import Flask
-from pymongo import MongoClient
+from datetime import datetime
+
+from flask import Flask, jsonify, request
+from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017, username='User', password='4lNukg1PfxSL8SZj')
-db = client['UniStud']
-collection = db['sample_mflix']
+uri = "mongodb+srv://User:4lNukg1PfxSL8SZj@unistud.mhdzsny.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    data = collection.find('movies')
-    return 'Hello World!'
+def get_events():
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    events = client.unities.events.find()
+    events_list = []
+
+    for event in events:
+        event_data = {
+            'name': event['name'],
+            'description': event['description'],
+        }
+        events_list.append(event_data)
+
+    return jsonify({'events': events_list})
+
+
+@app.route('/events')
+def add_movie():
+    date_string = "2023-11-17"
+    formatted_date1 = datetime.strptime(date_string, "%Y-%m-%d").date()
+    date_string = "2023-11-19"
+    formatted_date2 = datetime.strptime(date_string, "%Y-%m-%d").date()
+    try:
+        event_data = jsonify({
+            'name': 'Unihack',
+            'description': 'This is a fun hackathon!',
+            'county': 'Timis',
+            'open': True,
+            'accommodation': True,
+            'dateStart': formatted_date1,
+            'dateEnd': formatted_date2,
+            'organizer': 'Liga AC',
+            'imageUrl': ''
+        })
+
+        # Insert the movie into the collection
+        result = client.unities.events.insert_one(event_data)
+
+        # Check if the insertion was successful
+        if result.inserted_id:
+            return jsonify({'message': 'Event added successfully', 'event_id': str(result.inserted_id)}), 201
+        else:
+            return jsonify({'error': 'Failed to add event'}), 500
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
